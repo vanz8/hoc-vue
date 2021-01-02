c5c257c5a1ae0c51ded55ee8d84c625885187be3
vannn1604 <vannn1604@gmail.com>
2021-01-03 00:58:52 +0700

    homepage

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,33 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+  <a-layout id="components-layout-demo-top" class="layout">
+    <a-layout-header :style="{paddingTop: '4px'}">
+      <div :style="{marginLeft: '15px', float: 'left'}">
+        <a href="/" :style="{color: '#fff', fontWeight: 500}">KREATIVE</a>
+      </div>
+      <a-menu
+        theme="dark"
+        mode="horizontal"
+        :style="{ lineHeight: '58px', float: 'right', paddingTop: '2px' }"
+      >
+        <a-menu-item key="1">
+          Contact
+        </a-menu-item>
+        <a-menu-item key="2">
+          Login
+        </a-menu-item>
+      </a-menu>
+    </a-layout-header>
+    <a-layout-content style="padding: 0 50px">
+      <div
+        :style="{ background: '#fff', padding: '24px', margin: '16px 0' }"
+      >
+        <router-view />
+      </div>
+    </a-layout-content>
+    <a-layout-footer :style="{ textAlign: 'center' }">
+      Ant Design ©2018 Created by Ant UED
+    </a-layout-footer>
+  </a-layout>
 </template>
 
 <script>
@@ -10,14 +35,3 @@ export default {
   name: 'App'
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import HomePage from '@/components/HomePage'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
+      path: '/hello-world',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/',
+      name: 'HomePage',
+      component: HomePage
     }
   ]
 })
